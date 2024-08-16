8029ff2993e301339790ccfd000c835954211835
IoannaB04 <ioannabourcha28@hotmail.com>
2024-08-16 14:14:25 +0200

    utils for selfsupervision

---
+++ utils/pose_utils.py
@@ -0,0 +1,131 @@
+# Adapt from https://github.com/TRI-ML/packnet-sfm/blob/de53b310533ff6b01eaa23a8ba5ac01bac5587b1/packnet_sfm/geometry/pose_utils.py
+
+import torch
+import numpy as np
+
+def rot_from_axisangle(vec):
+    """Convert an axisangle rotation into a 4x4 transformation matrix
+    (adapted from https://github.com/Wallacoloo/printipi)
+    Input 'vec' has to be Bx1x3
+    """
+    angle = torch.norm(vec, 2, 2, True)
+    axis = vec / (angle + 1e-7)
+
+    ca = torch.cos(angle)
+    sa = torch.sin(angle)
+    C = 1 - ca
+
+    x = axis[..., 0].unsqueeze(1)
+    y = axis[..., 1].unsqueeze(1)
+    z = axis[..., 2].unsqueeze(1)
+
+    xs = x * sa
+    ys = y * sa
+    zs = z * sa
+    xC = x * C
+    yC = y * C
+    zC = z * C
+    xyC = x * yC
+    yzC = y * zC
+    zxC = z * xC
+
+    #rot = torch.zeros((vec.shape[0], 3, 3)).to(device=vec.device)
+    rot = torch.zeros((vec.shape[0], 4, 4)).to(device=vec.device)
+
+    rot[:, 0, 0] = torch.squeeze(x * xC + ca)
+    rot[:, 0, 1] = torch.squeeze(xyC - zs)
+    rot[:, 0, 2] = torch.squeeze(zxC + ys)
+    rot[:, 1, 0] = torch.squeeze(xyC + zs)
+    rot[:, 1, 1] = torch.squeeze(y * yC + ca)
+    rot[:, 1, 2] = torch.squeeze(yzC - xs)
+    rot[:, 2, 0] = torch.squeeze(zxC - ys)
+    rot[:, 2, 1] = torch.squeeze(yzC + xs)
+    rot[:, 2, 2] = torch.squeeze(z * zC + ca)
+    rot[:, 3, 3] = 1
+
+    return rot
+
+def get_translation_matrix(translation_vector):
+    """Convert a translation vector into a 4x4 transformation matrix
+    """
+    T = torch.zeros(translation_vector.shape[0], 4, 4).to(device=translation_vector.device)
+
+    t = translation_vector.contiguous().view(-1, 3, 1)
+
+    T[:, 0, 0] = 1
+    T[:, 1, 1] = 1
+    T[:, 2, 2] = 1
+    T[:, 3, 3] = 1
+    T[:, :3, 3, None] = t
+
+    return T
+
+def euler2mat(angle):
+    """Convert euler angles to rotation matrix"""
+    B = angle.size(0)
+    x, y, z = angle[:, 0], angle[:, 1], angle[:, 2]
+
+    cosz = torch.cos(z)
+    sinz = torch.sin(z)
+
+    zeros = z.detach() * 0
+    ones = zeros.detach() + 1
+    zmat = torch.stack([cosz, -sinz, zeros,
+                        sinz, cosz, zeros,
+                        zeros, zeros, ones], dim=1).view(B, 3, 3)
+
+    cosy = torch.cos(y)
+    siny = torch.sin(y)
+
+    ymat = torch.stack([cosy, zeros, siny,
+                        zeros, ones, zeros,
+                        -siny, zeros, cosy], dim=1).view(B, 3, 3)
+
+    cosx = torch.cos(x)
+    sinx = torch.sin(x)
+
+    xmat = torch.stack([ones, zeros, zeros,
+                        zeros, cosx, -sinx,
+                        zeros, sinx, cosx], dim=1).view(B, 3, 3)
+
+    rot_mat = xmat.bmm(ymat).bmm(zmat)
+    pose = torch.eye(4, device=rot_mat.device, dtype=rot_mat.dtype).repeat([len(rot_mat), 1, 1])
+    pose[:, :3, :3] = rot_mat[:, :3, :3]
+    return pose
+
+
+def pose_vec2mat(vec, mode='euler', invert=False):
+    """Convert Euler parameters to transformation matrix."""
+    if mode is None:
+        return vec
+    trans, rot = vec[:, :3].unsqueeze(-1), vec[:, 3:]
+    if mode == 'euler':
+        R = euler2mat(rot)
+    elif mode == 'rodrigues':
+        R = rot_from_axisangle(rot.unsqueeze(1))
+    else:
+        raise ValueError('Rotation mode not supported {}'.format(mode))
+    t = trans.clone()
+    if invert:
+        R = R.transpose(1, 2)
+        t *= -1
+    T = get_translation_matrix(t)
+    mat = R @ T if invert else T @ R
+    #mat = torch.cat([R, t], dim=2)  # [B,3,4]
+    return mat[:, :3, :]
+
+
+def invert_pose(T):
+    """Inverts a [B,4,4] torch.tensor pose"""
+    Tinv = torch.eye(4, device=T.device, dtype=T.dtype).repeat([len(T), 1, 1])
+    Tinv[:, :3, :3] = torch.transpose(T[:, :3, :3], -2, -1)
+    Tinv[:, :3, -1] = torch.bmm(-1. * Tinv[:, :3, :3], T[:, :3, -1].unsqueeze(-1)).squeeze(-1)
+    return Tinv
+
+
+def invert_pose_numpy(T):
+    """Inverts a [4,4] np.array pose"""
+    Tinv = np.copy(T)
+    R, t = Tinv[:3, :3], Tinv[:3, 3]
+    Tinv[:3, :3], Tinv[:3, 3] = R.T, - np.matmul(R.T, t)
+    return Tinv
